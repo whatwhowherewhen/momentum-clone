10ba782463072333ce8948db58ee4d5617196aba
Hyoshin Kim <gimhyoshin@gmail.com>
2024-07-03 18:12:18 +0900

    로그인 정보를 localStorage에 저장해서 유지

--- app.js
+++ app.js
@@ -2,22 +2,45 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
-const HIDDEN_CLASSNAME = "hidden"; //스트링만 포함된 변수는 대문자로 관습. 
+const HIDDEN_CLASSNAME = "hidden"; //스트링만 포함된 변수는 대문자로 관습.
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY); //지역변수 username 값과 동일할 것임
+
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`; //위와 같은데 백틱기호임. 이게 더 편한듯
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  //사실 파라미터 username이름은 중요하진 않음
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-/* 1.html에서 login-form 밑에 h1이라는 블록을 만들고 히든 클래스를 추가함.
-2. 히든 클래스는 css스타일에 display: none으로 정의됨.
-3. js에서 html의 h1 정보를 가져오는 greeting 변수 생성
-4. username에 input value를 저장하고 loginForm에 히든 클래스 추가하여 로그인 폼 사라지게끔
-5. 그리고 h1을 나타내는 greeting의 innerText에 인사+변수값을 주고
-6. 보이지 않던 greeting (h1)을 히든 클래스 리무브로 나타나게 함.
-7. 이 모든 것은 로그인 폼이 submit되었을 때 일어남   */
+if (savedUsername === null) {
+  // "null" 아님 주의
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
+
+/* 
+loginForm에서 submit이 되면 onLoginSubmit 함수가 호출된다. 이때 파라미터를 event라고 해놓고 event 객체를 이용해 브라우저 기본행동을 preventDefault()로 막아놓는다. username 지역변수를 하나 만들고 여기에 loginInput.value을 저장한다. 
+
+localstorage에 USERNAME_KEY 라는 이름의 키를 만들고 거기에 지역변수 username에 저장된 값을 넣는다.
+그리고 로그인 폼에 히든 클래스를 추가하며 안보이게 만들고 paintGreeting함수를 아규먼트 username으로 호출한다. 
+
+paintGreeting은 h1을 보이게 하는 함수이다. greeting에 히든 클래스를 삭제하고 안에다가 텍스트를 준다. 
+
+localstorage는 웹 창을 새로고침하던, 콘솔창을 새로하던 브라우저 안의 storage로 지워지지 않는다. 만약 savedUsername 값이 존재한다면 바로 paintGreeting 호출하고, 비어있으면 로그인 폼 보인다.
+
+여기서는 아규먼트 savedUsername으로 호출된다. savedUsername은 local storage의 키인 USERNAME_KEY의 value을 말한다. (지역변수 username과 동일)
+
+*/
