3ac00ff6c8d75671e91b19384f9a36cd98e50a12
Hyoshin Kim <gimhyoshin@gmail.com>
2024-07-04 14:46:42 +0900

    시계 수정

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,15 @@ const clock = document.querySelector("h2#clock"); //h2 #clock은 XgetClock
 function getClock() {
   /* Date는 기본적으로 제공되는 생성자 함수이다. 그냥 Date();를 하면 현재 날짜와 시간을 문자열로 반환하지만 new Date(); 할 경우에는 새로운 Date 객체로 반환한다. */
   const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  //String으로 감싸서 숫자형을 문자형으로 바꿈
 
-  
-   clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  /* 문자열.padStagrt(문자열길이설정, "앞에 채울문자")
+문자열의 길이가 설정보다 작으면 앞쪽을 문자로 채움 */
 
-   /* date.getHours()는 객체 속 메서드이며 숫자 형태로 값을 반환한다. ":" 안하려고 이렇게 템플릿 리터럴 로 만들어 date.getHours()의 결과를 문자열로 포함시켰다. */
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
