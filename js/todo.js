357050a5d882c0aea315ea797c319298aa02fe83
Hyoshin Kim <gimhyoshin@gmail.com>
2024-07-05 15:40:19 +0900

    local storage 저장

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,17 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  /* localStorage.setItem("todos", toDos); //localstorage 하나의 value에 a, b, c로 저장됨. */
+  localStorage.setItem("todos", JSON.stringify(toDos));
+  // localstrage 하나의 value에 ["a", "b", "c"] 로 저장됨. 사용법만 알겠다..?
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  /* submit되고 paintToDo가 호출될 때 새로운 li가 생겨나고
-      span과 버튼이 li 속에 생겨난다.
-      버튼이 클릭될 때 많은 정보를 아규먼트 event로 하여 deleteToDo 호출하고
-      event 정보 속 target 안에 parentElement라는게 있으며 이는 해당하는 li 요소 이다.
-     따라서 각 버튼을 누름으로써 li 단위로 삭제할 수 있는 것 */
-  li.remove();
+  remove(li);
 }
 function paintToDo(newTodo) {
   const li = document.createElement("li");
@@ -27,7 +30,8 @@ function paintToDo(newTodo) {
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
-  toDoInput.value = ""; //newTodo 에는 영향 없어
+  toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
 }
 
