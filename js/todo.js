1632ef3d1df3ea948179075ebde4b1057a0197c5
Hyoshin Kim <gimhyoshin@gmail.com>
2024-07-05 13:52:31 +0900

    todo first

---
+++ js/todo.js
@@ -0,0 +1,20 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = ""; //newTodo 에는 영향 없어
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
