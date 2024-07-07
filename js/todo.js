ac714a0f7f4560915361a14c7c154f100c78c274
Hyoshin Kim <gimhyoshin@gmail.com>
2024-07-07 15:13:13 +0900

    todo list  완성

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,7 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -14,13 +14,19 @@ function saveToDos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  li.remove();
+  li.remove(); //UI에서만 사라지고 li 변수는 존재한다..
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  //여기 (toDo) 이름은 뒤 조건에 맞게 toDos 배열을 변형한 새로운 배열이다(기존배열은 그대로 두고)
+  //li.id 는 문자열이고 toDo.id는 숫자형이다.
+  saveToDos(); // 이거까지 해야 localstorage도 업데이트되겠지
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id; //근데 string 타입으로 저장되네. 주의
+  // html element에 한해서 모든 속성 값이 문자열만 허용한다네.
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -34,21 +40,39 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
   saveToDos();
-  paintToDo(newTodo);
+  paintToDo(newTodoObj);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
-/* function sayHello(item) {
-  console.log("This is turn of ", item);
-} */
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos != null) {
   const parsedToDos = JSON.parse(savedToDos);
   /*  parsedToDos.forEach(sayHello); */
-  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
+
+/* filter function 은 원래 배열에서 조건에 따라 몇 개 제외하고 새로운 배열을 제공한다.
+function sexyFilter(item){return item !== 3} -> item이 3이 아니면 true를 return!
+[1,2,3,4,5].filter(sexyFilter)
+(4) [1, 2, 4, 5]
+
+const arr = ["pizza", "banana", "tomato"] -> arr 배열에서 banana를 지우고 싶음!
+function sexyFilter(food){return food !== "banana"}
+arr.filter(sexyFilter)
+(2) ['pizza', 'tomato']
+
+const todos = [{text:"lalala"}, {text:"lololo"}]
+function sexyFilter(todo){return todo.text != "lololo"}
+todos.filter(sexyFilter)
+[{…}]0: {text: 'lalala'} length: 1 [[Prototype]]: Array(0)
+
+*/
